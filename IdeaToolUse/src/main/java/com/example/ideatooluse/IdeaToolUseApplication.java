4baead821f1ced0d5c5a848e8eb0ed2dcbe22fa5
xinxing <2216252906@qq.com>
2022-08-30 17:37:48 +0800

    增加文件IdeaToolUseApplication.java IdeaToolUseApplicationTests.java Test0830.java pom.xml

---
+++ IdeaToolUse/src/main/java/com/example/ideatooluse/IdeaToolUseApplication.java
@@ -0,0 +1,14 @@
+package com.example.ideatooluse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaToolUseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaToolUseApplication.class,
+                args);
+    }
+
+}
---
+++ IdeaToolUse/src/test/java/com/example/ideatooluse/Test0830.java
@@ -0,0 +1,7 @@
+package com.example.ideatooluse;
+
+public class Test0830 {
+    public static void main(String[] args) {
+
+    }
+}
